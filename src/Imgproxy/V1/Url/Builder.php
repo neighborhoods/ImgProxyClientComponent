83df11e4250e7c28416ac3a452f0fa1de11c4525
Nathaniel Alvarado <nathaniel.alvarado@55places.com>
2020-06-12 15:38:44 -0500

    Add test

--- src/Imgproxy/V1/Url/Builder.php
+++ src/Imgproxy/V1/Url/Builder.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Neighborhoods\ImgProxyClientComponent\Imgproxy\V1\Url\Url;
+namespace Neighborhoods\ImgProxyClientComponent\Imgproxy\V1\Url;
 
 use Neighborhoods\ImgProxyClientComponent\Imgproxy\V1\UrlInterface;
 
--- test/Unit/SignImageTest.php
+++ test/Unit/SignImageTest.php
@@ -1,10 +1,33 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use Neighborhoods\ImgProxyClientComponent\Imgproxy\V1\Url;
-use Neighborhoods\ImgProxyClientComponent\Imgproxy\V1\UrlBuilder;
+//use Neighborhoods\ImgProxyClientComponent\Imgproxy\V1\Url;
+use Neighborhoods\ImgProxyClientComponent\Imgproxy\V1\Url\Builder;
 
 class SignImageTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function shouldBuildSignedImage()
+    {
+        $urlBuilder = new Neighborhoods\ImgProxyClientComponent\Imgproxy\V1\Url\Builder();
+        $imgproxyV1UrlFactory = new \Neighborhoods\ImgProxyClientComponent\Imgproxy\V1\Url\Factory();
+        $imgproxyV1UrlFactory->setImgproxyV1Url(new \Neighborhoods\ImgProxyClientComponent\Imgproxy\V1\Url());
 
+        $urlBuilder->setSalt('Fake Salt')
+            ->setKey('Fake Key')
+            ->setSecure(true)
+            ->setImageUrl('https://nhds-cms-service-prod.s3.amazonaws.com/career-page/private/1.jpg')
+            ->setFit('fit')
+            ->setGravity('sm')
+            ->setEnlarge(false)
+            ->setExtension('null')
+            ->setHeight(200)
+            ->setWidth(300);
+
+        $url = $urlBuilder->setImgproxyV1UrlFactory($imgproxyV1UrlFactory)->build();
+
+        $this->assertEquals('/0MBh4uk0iRF0HyMbHZlaZXY_RbnWC-g1QIKu-WYPz6s/fit/300/200/sm/0/aHR0cHM6Ly9uaGRzLWNtcy1zZXJ2aWNlLXByb2QuczMuYW1hem9uYXdzLmNvbS9jYXJlZXItcGFnZS9wcml2YXRlLzEuanBn.null', $url->getSecureSignedPath());
+    }
 }
